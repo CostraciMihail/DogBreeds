31fd0c7b8dfcc7232c80f4d8fef4e50722a78ba7
MihailC <roller.sl@mail.com>
2021-03-18 18:43:50 +0200

    Added: default flutter route screen for AppClip.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,24 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
     GeneratedPluginRegistrant.register(with: self)
+    
+    #if APPCLIP
+    let flutterVC = FlutterViewController(project: nil,
+                                          initialRoute: "/main-screen",
+                                          nibName: nil,
+                                          bundle: nil)
+    
+    window.rootViewController = flutterVC
+    window.makeKeyAndVisible()
+    #endif
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
